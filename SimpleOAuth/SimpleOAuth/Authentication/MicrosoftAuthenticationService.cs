f7cc0180e387607fd80434935b1144d801797c5f
Thomas Zeller <tze@ais.de>
2022-10-01 00:03:55 +0200

    adds some more props and fills out the service as in google

--- SimpleOAuth/SimpleOAuth/Authentication/MicrosoftAuthenticationService.cs
+++ SimpleOAuth/SimpleOAuth/Authentication/MicrosoftAuthenticationService.cs
@@ -4,19 +4,56 @@ using SimpleOAuth.ConfigurationModel;
 
 public class MicrosoftAuthenticationService : IAuthenticationService
 {
+    // Based on https://learn.microsoft.com/de-de/azure/active-directory/develop/id-tokens
+    // v1.0 => Azure AD endpoint NOT USED HERE
+    // v2.0 => MS Identity Platform (used)
+    // v2.0 => permissions and consent https://learn.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent 
+    private const string _basePath = "login.microsoftonline.com";
+    private readonly MicrosoftOAuthClientConfiguration _microsoftOAuthClientConfiguration;
+    private readonly UriBuilder _uriBuilder;
+
+    // allowed scopes openid, email, profile, offline_access
+    public string[] AllowedScoped { get; private set; }
+
     public Provider Provider => Provider.Microsoft;
 
     public MicrosoftAuthenticationService(MicrosoftOAuthClientConfiguration microsoftOAuthClientConfiguration)
     {
-
+        AllowedScoped = new string[] { "openid", "email" };
+        _microsoftOAuthClientConfiguration = microsoftOAuthClientConfiguration;
+        _uriBuilder = SetUri();
     }
     public Uri AuthUri(params string[] scope)
     {
-        throw new NotImplementedException();
+        var scopes = string.Join(" ", scope.ToArray());
+
+        return _uriBuilder.Uri.AddParamerteCollection(BaseConfigurationSets).AddParameter("scope", scopes);
+    }
+
+    public override string ToString()
+    {
+        return _uriBuilder.Uri.AbsolutePath;
     }
 
     public Dictionary<string, string> GetUserInfo()
     {
         throw new NotImplementedException();
+    }
+
+    private Dictionary<string, string> BaseConfigurationSets => new Dictionary<string, string>
+        {
+            {"client_id", _microsoftOAuthClientConfiguration.ClientId},
+            {"tenant", _microsoftOAuthClientConfiguration.Tenant!},
+            {"redirect_uri",_microsoftOAuthClientConfiguration!.RedirectUrl!}
+        };
+
+    private UriBuilder SetUri()
+    {
+        return new UriBuilder
+        {
+            Scheme = "https",
+            Host = _basePath,
+            Path = "common/oauth2/v2.0/authorize"
+        };
     }
 }
--- SimpleOAuth/SimpleOAuth/ConfigurationModel/MicrosoftOAuthClientConfiguration.cs
+++ SimpleOAuth/SimpleOAuth/ConfigurationModel/MicrosoftOAuthClientConfiguration.cs
@@ -3,15 +3,18 @@
     /// <summary>
     /// based on https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-client-creds-grant-flow#first-case-access-token-request-with-a-shared-secret
     /// </summary>
-    public class MicrosoftOAuthClientConfiguration : OAuthClientConfiguration
+    public class MicrosoftOAuthClientConfiguration : OAuthClientBaseConfiguration
     {
-        public MicrosoftOAuthClientConfiguration(string clientId, string clientSecret, string tenant, string scope) : base(clientId, clientSecret)
+        public MicrosoftOAuthClientConfiguration(string clientId, string clientSecret, string tenant, string scope, string redirectUrl) : base(clientId, clientSecret)
         {
             Tenant = tenant;
             Scope = scope;
+            RedirectUrl = redirectUrl;
         }
 
         public string Tenant { get; } // TODO : Guid or domain-name format! -> Validation
+        public string RedirectUrl { get; }
+        public string? State { get; set; } //optional, kind of a tag that you get also in return
         public string Scope { get; } // more about the scope here : https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#the-default-scope
         public string GrantType { get; } = "client_credentials"; // Must be set to client_credentials | https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-client-creds-grant-flow#first-case-access-token-request-with-a-shared-secret
     }
